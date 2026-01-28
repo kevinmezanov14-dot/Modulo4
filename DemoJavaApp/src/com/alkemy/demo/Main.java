612a9c967d1f7d2f60e6dfa4afc81dd65263d6fb
Kevin Meza Catril <kevin.meza.nov14@gmail.com>
2026-01-28 11:39:30 -0300

    inicio proyecto java

---
+++ DemoJavaApp/src/com/alkemy/demo/Main.java
@@ -0,0 +1,14 @@
+package com.alkemy.demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// escribir en consola
+		System.out.println("Hola mundo");
+		System.err.println("Error");
+		System.out.print(0);
+		System.out.print("Main.main()");
+		System.out.print("otro texto");
+	}
+
+}
